b271ecfdacea49d92486f1c316ac0df61f168a30
Laurence Rouesnel <laurence@rouesnel.com>
2015-09-06 19:47:48 +1000

    Disabled doctests (for now because of ambiguous module name issues).

--- test/DocTests.hs
+++ test/DocTests.hs
@@ -2,5 +2,6 @@ module Main where
 
 import Test.DocTest
 
+-- Disabled for now because Stack support doesn't work properly (Ambiguous module name errors).
 main :: IO ()
-main = doctest ["src"]
+main = doctest ["-isrc"]
